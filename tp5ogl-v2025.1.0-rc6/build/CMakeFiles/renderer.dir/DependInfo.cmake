4b54f4d1c57e639075b5121806ce29c7038771c6
unknown <yygolivia@gmail.com>
2025-06-17 11:16:41 +0200

    finish

---
+++ tp5ogl-v2025.1.0-rc6/build/CMakeFiles/renderer.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/MeshModel.cpp" "CMakeFiles/renderer.dir/src/MeshModel.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/MeshModel.cpp.o.d"
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/core.cpp" "CMakeFiles/renderer.dir/src/core.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/core.cpp.o.d"
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/geometry.cpp" "CMakeFiles/renderer.dir/src/geometry.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/geometry.cpp.o.d"
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/loop.cpp" "CMakeFiles/renderer.dir/src/loop.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/loop.cpp.o.d"
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/objReader.cpp" "CMakeFiles/renderer.dir/src/objReader.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/objReader.cpp.o.d"
+  "/mnt/e/Desktop/tp5ogl-v2025.1.0-rc6/src/rendering.cpp" "CMakeFiles/renderer.dir/src/rendering.cpp.o" "gcc" "CMakeFiles/renderer.dir/src/rendering.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tp5ogl-v2025.1.0-rc6/build/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/src/MeshModel.cpp.o"
+  "CMakeFiles/renderer.dir/src/MeshModel.cpp.o.d"
+  "CMakeFiles/renderer.dir/src/core.cpp.o"
+  "CMakeFiles/renderer.dir/src/core.cpp.o.d"
+  "CMakeFiles/renderer.dir/src/geometry.cpp.o"
+  "CMakeFiles/renderer.dir/src/geometry.cpp.o.d"
+  "CMakeFiles/renderer.dir/src/loop.cpp.o"
+  "CMakeFiles/renderer.dir/src/loop.cpp.o.d"
+  "CMakeFiles/renderer.dir/src/objReader.cpp.o"
+  "CMakeFiles/renderer.dir/src/objReader.cpp.o.d"
+  "CMakeFiles/renderer.dir/src/rendering.cpp.o"
+  "CMakeFiles/renderer.dir/src/rendering.cpp.o.d"
+  "librenderer.pdb"
+  "librenderer.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
